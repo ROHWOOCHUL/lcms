c26f0e80092d2e50e95dd77cde068374e0c47ead
이준희 <leejh95@nate.com>
2023-01-04 19:02:56 +0900

    Rtm 작업 WIP

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,55 +1,102 @@
-import AgoraRTC, { ILocalAudioTrack, ILocalVideoTrack } from "agora-rtc-sdk-ng";
-import React, { ReactElement, useRef, useState } from "react";
-import { useClient, useStart, useUsers } from "../GlobalContext";
+import { FaMicrophone, FaVideo } from "react-icons/fa";
+import { useAdmin, useClientContext } from "../GlobalContext";
+import { useEffect, useRef, useState } from "react";
 
+import { MdMonitor } from "react-icons/md";
 import { User } from "../types";
+import { createScreenVideoTrack } from "agora-rtc-react";
 
 interface Props {
   user: User;
   vidDiv: any;
+  action: () => Promise<void>;
 }
 
-const Controls = (props: Props) => {
+const Controls = ({ user, action }) => {
   const [sharingScreen, setSharingScreen] = useState<boolean>(false);
-  const setStart = useStart()[1];
-  const setUsers = useUsers()[1];
-  const rtc = useClient();
-  const localScreenTracks = useRef<
-    ILocalVideoTrack | [ILocalVideoTrack, ILocalAudioTrack] | null | any
-  >(null);
-  const leaveChannel = async () => {
-    // Destroy the local audio and video tracks.
-    await rtc.current.localAudioTrack.close();
-    await rtc.current.localVideoTrack.close();
-    await rtc.current.client.leave();
-    setUsers([]);
-    setStart(false);
+  const admin = useAdmin();
+  const client = useClientContext();
+  const localScreenTracks = useRef<any>(null);
+  // localScreenTracks.current = createScreenVideoTrack({});
+  const useScreenVideoTrack = createScreenVideoTrack({});
+  const { ready, tracks, error } = useScreenVideoTrack();
+  const muteMaster = (type) => {
+    if (user.client) {
+      action(type);
+    } else {
+      if (admin.current) {
+        messageMaster(type);
+      }
+    }
   };
 
-  const mute = (type: "video" | "audio", id: string) => {
-    if (type === "audio") {
-      setUsers((prevUsers: User[]) => {
-        return prevUsers.map((user) => {
-          if (user.uid === id) {
-            user.client && rtc.current.localAudioTrack.setEnabled(!user.audio);
-            return { ...user, audio: !user.audio };
-          }
-          return user;
-        });
-      });
-    } else if (type === "video") {
-      // setUsers((prevUsers: User[]) => {
-      //   return prevUsers.map((user) => {
-      //     if (user.uid === id) {
-      //       user.client && rtc.current.localVideoTrack.setEnabled(!user.video);
-      //       return { ...user, video: !user.video };
-      //     }
-      //     return user;
-      //   });
-      // });
-      rtc.current.localVideoTrack._originMediaStreamTrack.enabled =
-        !rtc.current.localVideoTrack._originMediaStreamTrack.enabled;
+  useEffect(() => {
+    if (sharingScreen) {
+      shareScreen();
+    } else {
+      stopShareScreen();
     }
+  }, [sharingScreen]);
+
+  const messageMaster = (type) => {
+    console.log(type);
+    client.current.rtm.client.sendMessageToPeer({ text: type }, user.username);
+  };
+
+  const buildClassName = (type) => {
+    let str = "";
+    str += type === "audio" && user.audio ? "on " : "";
+    str += type === "video" && user.video ? "on " : "";
+    str += !admin.current && !user.client ? "noClick" : "";
+    return str;
+  };
+
+  // 화면 공유는 사운드가 없는 것 같다.
+  const shareScreen = async () => {
+    console.log(tracks);
+    // local 환경에서 커스텀 불가능 ??
+    // {
+    //   encoderConfig: {
+    //     width: { min: 640, max: 1920 },
+    //     height: { min: 480, max: 1080 },
+    //   },
+    // },
+    // "enable"
+
+    // Video 비활성화
+    user.videoTrack.stop();
+    // 화면공유 화면 실행
+    localScreenTracks.current.play(tracks);
+
+    //스크린 공유 종료 이벤트 리스너
+    // 이벤트 종료 리스너는 스크린 공유를 선언한 위치에서만 선언할 수 있다. (왜 그런지는 모름)
+    localScreenTracks.current.on("track-ended", async () => {
+      console.log("트래킹 종료");
+      stopShareScreen();
+      setSharingScreen(false);
+    });
+    await client.rtc.current.client.unpublish(
+      client.rtc.current.localVideoTrack
+    );
+    await client.rtc.current.client.publish([localScreenTracks.current]);
+
+    // document.querySelector(".agora_video_player").style.objectFit = "contain";
+    // console.log(document.querySelector(".agora_video_player"));
+  };
+
+  const stopShareScreen = async (): Promise<void> => {
+    console.log("화면 공유 중지", localScreenTracks.current);
+    // 화면공유 화면 비활성화
+    localScreenTracks.current &&
+      localScreenTracks.current.stop(user.videoTrack);
+    // Video 활성화
+    user.videoTrack.play(user.videoTrack);
+
+    await client.rtc.current.client.unpublish([localScreenTracks.current]);
+    await client.rtc.current.client.publish([
+      client.rtc.current.localAudioTrack,
+      client.rtc.current.localVideoTrack,
+    ]);
   };
 
   const toggleScreen = async () => {
@@ -62,177 +109,35 @@ const Controls = (props: Props) => {
     }
   };
 
-  // 화면 공유는 사운드가 없는 것 같다.
-  const shareScreen = async () => {
-    // local 환경에서 커스텀 불가능 ??
-    // {
-    //   encoderConfig: {
-    //     width: { min: 640, max: 1920 },
-    //     height: { min: 480, max: 1080 },
-    //   },
-    // },
-    // "enable"
-    localScreenTracks.current = await AgoraRTC.createScreenVideoTrack({});
-
-    // Video 비활성화
-    props.user.videoTrack.stop();
-    // 화면공유 화면 실행
-    localScreenTracks.current.play(props.vidDiv.current);
-
-    //스크린 공유 종료 이벤트 리스너
-    // 이벤트 종료 리스너는 스크린 공유를 선언한 위치에서만 선언할 수 있다. (왜 그런지는 모름)
-    localScreenTracks.current.on("track-ended", async () => {
-      console.log("트래킹 종료");
-      stopShareScreen();
-      setSharingScreen(false);
-    });
-    await rtc.current.client.unpublish(rtc.current.localVideoTrack);
-    await rtc.current.client.publish([localScreenTracks.current]);
-
-    // document.querySelector(".agora_video_player").style.objectFit = "contain";
-    // console.log(document.querySelector(".agora_video_player"));
-  };
-
-  const stopShareScreen = async (): Promise<void> => {
-    console.log("화면 공유 중지", localScreenTracks.current);
-    // 화면공유 화면 비활성화
-    localScreenTracks.current &&
-      localScreenTracks.current.stop(props.vidDiv.current);
-    // Video 활성화
-    props.user.videoTrack.play(props.vidDiv.current);
-
-    await rtc.current.client.unpublish([localScreenTracks.current]);
-    await rtc.current.client.publish([
-      rtc.current.localAudioTrack,
-      rtc.current.localVideoTrack,
-    ]);
-  };
-
-  const localPlayerStart = async () => {
-    // Create an audio track from a source file
-    const track = await AgoraRTC.createBufferSourceAudioTrack({
-      source:
-        "https://static.hodooenglish.com/hodooSchool/levelTest/SplashHodoo.mp3",
-    });
-
-    track.startProcessAudioBuffer({ loop: false });
-    track.play();
-    rtc.current.client.publish(track);
-  };
-
   return (
-    <div className="controls" style={{ width: "100%" }}>
+    <div className="controls">
       {
         <p
-          className={props.user.audio ? "on" : ""}
-          onClick={() => props.user.client && mute("audio", props.user.uid)}
+          className={buildClassName("audio")}
+          onClick={() => muteMaster("audio")}
         >
-          Mic
+          <FaMicrophone />
         </p>
       }
       {
         <p
-          className={props.user.video ? "on" : ""}
-          onClick={() => props.user.client && mute("video", props.user.uid)}
+          className={buildClassName("video")}
+          onClick={() => muteMaster("video")}
         >
-          Video
+          <FaVideo />
         </p>
       }
-      {props.user.client && (
-        <div
-          style={{
-            height: "30px",
-            backgroundColor: "tomato",
-            color: "white",
-            cursor: "pointer",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            padding: "5px",
-            fontSize: "13px",
-            borderRadius: "50px",
-            fontWeight: 500,
-          }}
-          onClick={toggleScreen}
-        >
-          {sharingScreen ? "화면 공유 / 종료" : "화면 공유 / 시작"}
-        </div>
+      {
+        <p className={buildClassName("video")} onClick={toggleScreen}>
+          <MdMonitor />
+        </p>
+      }
+      {admin.current && !user.client ? (
+        <p onClick={() => messageMaster("kick")}>Kick</p>
+      ) : (
+        ""
       )}
-      <div style={{ display: "flex" }}>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-start",
-            marginRight: "10px",
-          }}
-        >
-          <label
-            style={{ color: "white", fontSize: "14px", marginBottom: "8px" }}
-          >
-            Local Audio Level
-          </label>
-          <input
-            type="range"
-            min="0"
-            id="localAudioVolume"
-            max="100"
-            step="1"
-            onChange={(e) => {
-              console.log("Volume of local audio :" + e.target.value);
-              // Set the local audio volume.
-              rtc.current.localAudioTrack.setVolume(parseInt(e.target.value));
-            }}
-          />
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-start",
-          }}
-        >
-          <label
-            style={{ color: "white", fontSize: "14px", marginBottom: "8px" }}
-          >
-            Remote Audio Level
-          </label>
-          <input
-            type="range"
-            min="0"
-            id="remoteAudioVolume"
-            max="100"
-            step="1"
-            onChange={(e) => {
-              console.log("Volume of remote audio :" + e.target.value);
-              // Set the remote audio volume.
-              localScreenTracks.current.remoteAudioTrack.setVolume(
-                parseInt(e.target.value)
-              );
-            }}
-          />
-        </div>
-      </div>
-      {
-        <button
-          type="button"
-          id="playAudioFile"
-          onClick={localPlayerStart}
-          style={{
-            height: "40px",
-            border: "none",
-            padding: "10px",
-            backgroundColor: "tomato",
-            color: "white",
-            borderRadius: "50px",
-            fontWeight: 500,
-            cursor: "pointer",
-          }}
-        >
-          Play audio file
-        </button>
-      }
-      {props.user.client && <p onClick={() => leaveChannel()}>나가기</p>}
+      {user.client && <p onClick={() => action("leave")}>Quit</p>}
     </div>
   );
 };
